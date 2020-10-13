37d7bd94b2a93f7734203734786a773451680bde
Debby Pulido <66884955+debbyps@users.noreply.github.com>
2020-10-12 23:43:34 -0700

    dps

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,5 @@
-d3.json('./data/samples.json').then(data =>{
+function  dropDownValues() {
+  d3.json('./data/samples.json').then(data =>{
     console.log(data);
 
     // get all the ids of the patients sampled
@@ -7,40 +8,17 @@ d3.json('./data/samples.json').then(data =>{
     
       ids.forEach(id =>{
         var dropDownOpt = d3.select("#selDataset").append("option").attr("value",id).text(id)
+        
       })
+      buildDetails(id);
+    })
+
+  function buildDetails(id) {
+    // get all the ids of the patients sampled
+    d3.json('./data/samples.json').then(data =>{
+      var metaDataDemographics = data.metadata.ethnicity;
+      // var samplesData = data.samples;  
+       console.log(metaDataDemographics)
+      //  console.log(samplesData)
+  })
 })
-
-
-
-// // Slice the first 10 objects for plotting
-// slicedData = sortedTopTen.slice(0, 10);
-
-// // Reverse the array to accommodate Plotly's defaults
-// reversedData = slicedData.reverse();
-
-// // Trace1 for the Greek Data
-// var trace1 = {
-//   x: reversedData.map(object => object.sample_values),
-//   y: reversedData.map(object => object.otu_ids),
-//   text: reversedData.map(object => object.otu_labels),
-//   name: "Bacteria",
-//   type: "bar",
-//   orientation: "h"
-// };
-
-// // data
-// var data = [trace1];
-
-// // Apply the group bar mode to the layout
-// var layout = {
-//   title: "Bacteria search results",
-//   margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//   }
-// };
-
-// // Render the plot to the div tag with id "plot"
-// Plotly.newPlot("plot", data, layout);
