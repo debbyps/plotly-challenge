0eab2b5ea17295860c92e0ee4a5478d2e15c24f6
Debby Pulido <66884955+debbyps@users.noreply.github.com>
2020-10-14 20:02:43 -0700

    dps

--- static/js/app_test.js
+++ static/js/app_test.js
@@ -1,33 +1,36 @@
-// d3.selectAll("#selDataset").on("change", optionChanged);
 // create an initiation to display info on html launch
 function init(){
     // use d3 to select the dropdown section of the html
     let dropDown = d3.select("#selDataset")
     // read the json data
-    d3.json('./data/samples.json').then(data =>{
+    d3.json('./data/samples.json').then((data) =>{
         // display all data in json on the console
-        // console.log(data);
+        console.log(data);
         // get all the ids of the patients sampled
-        let ids = Object.values(data.names)
-            // put into console just to see
-            console.log(ids);
+        // let ids = Object.values(data.names)
+        //     // put into console just to see
+        //     console.log(ids);
     // for each id in data names add an option into the dropdown
-            ids.forEach(id =>{
-            dropDown.append("option").attr("value",id).text(id)
+            data.names.forEach(id =>{
+                dropDown.append("option").attr("value",id).text(id)
       })
 
       // call the build details to pull demographic data
-       buildDemographics(940);
+       buildDemographics(id[0]);
+       buildTopTen(id[0]);
     })
   }  
 
-  function buildDemographics(ids) {
-     // get all the ids of the patients sampled
+
+  function buildDemographics(id) {
+     // read the json data
     d3.json('./data/samples.json').then(data =>{
-       var metaDataDemographics = data.metadata;
-       var finalArray = metaDataDemographics.filter(d => d.id === ids)[0]
-        console.log(metaDataDemographics)
-       var box = d3.select("#sample-metadata")
+       let metaDataDemographics = data.metadata;
+       // get all the ids of the patients sampled; had to change to a string
+       let finalArray = metaDataDemographics.filter(d => d.id.toString() === id)[0]
+       let box = d3.select("#sample-metadata")
+      // clear out info in demographics before new load
+        box.html("")
         box.append("h5").text(`id:${finalArray.id}`)
         box.append("h5").text(`ethnicity:${finalArray.ethnicity}`)
         box.append("h5").text(`gender:${finalArray.gender}`)
@@ -39,8 +42,21 @@ function init(){
    })
 }
 
-function optionChanged(ids){
-  buildDemographics()
+    function buildTopTen(id) {
+      // read the json data
+      d3.json('./data/samples.json').then(data =>{
+        // make sure to only grab the sample array for the id selected
+        let sampleData = data.samples.filter(d => d.id.toString() === id)[0]; 
+          console.log(sampleData)
+        // get only the values of the top ten
+        let ttFinalArray = sampleData.sample_values.slice(0,10)
+          console.log(ttFinalArray)
+          
+    })
+}
+function optionChanged(id){
+  buildDemographics(id)
+  buildTopTen(id)
 }
 
 init()
