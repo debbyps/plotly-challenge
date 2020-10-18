526292b5bebbc1e614b6c776683eeb84319352f0
Debby Pulido <66884955+debbyps@users.noreply.github.com>
2020-10-17 17:59:23 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # plotly-challenge
+Plot.ly Homework - Belly Button Biodiversity
+
+The purpose of this interactive dashboard is to explore the Belly Button Biodiversity dataset, which catalogs the microbes that colonize human navels.  
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+The dashboard consists of a dropdown for each test subject ID.  There are three sections: Demographic Info, Top Ten OTU's by ID and Frequency of OTU's by ID.  Upon launch, the dashboard will auto populate with the details pertaining to the first test subject.  On selection of the drop down, the dashboard will refresh for the corresponding test subject.
+
+The app.js has commented code which describes what each different section is doing.  The most challenging piece of this this was getting the demographics data to populate and getting the initial initiation to display the first test specimen data. 
--- static/js/app.js
+++ static/js/app.js
@@ -1,52 +1,146 @@
-// d3.selectAll("#selDataset").on("change", optionChanged);
+
 // create an initiation to display info on html launch
 function init(){
-  // use d3 to select the dropdown section of the html
-  let dropDown = d3.select("#selDataset")
-// read the json data
-  d3.json('./data/samples.json').then(data =>{
-    // display all data in json on the console
-    console.log(data);
-    // get all the ids of the patients sampled
-    let ids = Object.values(data.names)
-    // put into console just to see
-      console.log(ids);
-    // for each id in data names add an option into the dropdown
-      ids.forEach(id =>{
-        var dropDownOpt = dropDown.append("option").attr("value",id).text(id)
-      })
+    
+    // use d3 to select the dropdown section of the html
+    let dropDown = d3.select("#selDataset")
 
-      // call the build details to pull demographic data
-       buildDemographics(940);
+    // read the json data
+    d3.json('./data/samples.json').then((data) =>{
+ 
+        // display all data in json on the console
+        console.log(data);
+    // for each id in data names add an option into the dropdown
+        data.names.forEach(id =>{
+          dropDown.append("option").attr("value",id).text(id)
+          
+      })
+      
+      // call the build details to data and graphs
+      // have to use data.names[0] because this will give me the first value by index in array 
+      // for init to load w default data for first test subject id
+       buildDemographics(data.names[0]);
+       buildTopTen(data.names[0]);
+       buildBubble(data.names[0])
     })
   }  
 
-  function buildDemographics(ids) {
-     // get all the ids of the patients sampled
+
+  function buildDemographics(id) {
+ 
+     // read the json data
     d3.json('./data/samples.json').then(data =>{
-       var metaDataDemographics = data.metadata;
-       var mdID = metaDataDemographics.filter(d => d.id === ids)
-       var finalArray = mdID[0]
-       // var samplesData = data.samples;  
-        console.log(metaDataDemographics)
-       //  console.log(samplesData)
-       var box = d3.select("#sample-metadata")
-       box.append("h5").text(`id:${finalArray.id}`)
-       box.append("h5").text(`ethnicity:${finalArray.ethnicity}`)
-       box.append("h5").text(`gender:${finalArray.gender}`)
-       box.append("h5").text(`age:${finalArray.age}`)
-       box.append("h5").text(`location:${finalArray.location}`)
-       box.append("h5").text(`bbtype:${finalArray.bbtype}`)
-       box.append("h5").text(`wfreq:${finalArray.wfreq}`)
-       console.log(finalArray)
+       let metaDataDemographics = data.metadata;
+       // get all the ids of the patients sampled; had to change to a string
+       let finalArray = metaDataDemographics.filter(d => d.id.toString() === id)[0]
+       let box = d3.select("#sample-metadata")
+      // clear out info in demographics before new load
+        box.html("")
+        box.append("h5").text(`id:${finalArray.id}`)
+        box.append("h5").text(`ethnicity:${finalArray.ethnicity}`)
+        box.append("h5").text(`gender:${finalArray.gender}`)
+        box.append("h5").text(`age:${finalArray.age}`)
+        box.append("h5").text(`location:${finalArray.location}`)
+        box.append("h5").text(`bbtype:${finalArray.bbtype}`)
+        box.append("h5").text(`wfreq:${finalArray.wfreq}`)
+        console.log(finalArray)
    })
 }
 
-function optionChanged(ids){
-  buildDemographics()
+    function buildTopTen(id) {
+      // read the json data
+      d3.json('./data/samples.json').then(data =>{
+        // make sure to only grab the sample array for the id selected
+        let sampleData = data.samples.filter(d => d.id.toString() === id)[0]; 
+          console.log(sampleData)
+        // get only the values of the top ten reverse to get in desc ord know that in console it will look asc but on graph it will be desc
+        let ttValueArray = sampleData.sample_values.slice(0,10).reverse()
+          console.log(ttValueArray)
+        // get the ids that correspond to the values reverse to get it in desc ord know that in console it will look asc but on graph it will be desc
+        let ttIdsArray = sampleData.otu_ids.slice(0,10).reverse()
+          console.log(ttIdsArray)
+        // map back to otu id's so id won't be read as a number 
+        let ttOtuIds = ttIdsArray.map(d => "OTU " + d)
+          console.log(ttOtuIds)
+        // get the labels so when we hover over bar we know name of OTUs
+        let ttLabelArray = sampleData.otu_labels.slice(0,10)
+
+        // Trace1 for the top ten data
+        var trace1 = {
+          x: ttValueArray,
+          y: ttOtuIds,
+          text: ttLabelArray,
+          name: "Top Ten",
+          type: "bar",
+          orientation: "h"
+        };
+        // data
+        var data = [trace1];
+
+        // Apply the group bar mode to the layout
+        var layout = {
+          title: "Top Ten OTU's by ID",
+          yaxis: {
+            tickmode: "linear"
+          },
+          margin: {
+            l: 100,
+            r: 100,
+            t: 25,
+            b: 20
+          }
+        };
+
+        // Render the plot to the div tag with id "bar"
+        Plotly.newPlot("bar", data, layout);
+    })
 }
-  // d3.event.preventDefault();
-  
-// }
+function buildBubble(id) {
+  // read the json data
+  d3.json('./data/samples.json').then(data =>{
+    // make sure to only grab the sample array for the id selected
+    let sampleData = data.samples.filter(d => d.id.toString() === id)[0]; 
+    // get only the values
+    let ttValueArray = sampleData.sample_values
+    // get the ids that correspond to the values
+    let ttIdsArray = sampleData.otu_ids
+    // get the labels so when we hover over bar we know name of OTUs
+    let ttLabelArray = sampleData.otu_labels
+
+
+    // Trace2 for the bubble
+    var trace2 = {
+      y: ttValueArray,
+      x: ttIdsArray,
+      mode: "markers",
+      text: ttLabelArray,
+      marker: {
+        color: ttIdsArray,
+        opacity: [1, 0.8, 0.6, 0.4],
+        size: ttValueArray
+      }
+    };
+    // console.log(trace2)
+    // data
+    var data = [trace2];
+
+    // Apply the layout
+    var layout = {
+      title: "Frequency of OTU's by ID",
+      showlegend: false,
+      height: 600,
+      width: 1200
+    }
+
+    // Render the plot to the div tag with id "bubble"
+    Plotly.newPlot("bubble", data, layout);
+})
+}
+
+function optionChanged(id){
+  buildDemographics(id)
+  buildTopTen(id)
+  buildBubble(id)
+}
 
 init()
